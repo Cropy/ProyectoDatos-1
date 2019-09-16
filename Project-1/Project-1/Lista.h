e590108b84b5b2b8594c2d8a7490ecb472c9657d
CruzStark <christiandacruz04@gmail.com>
2019-09-15 18:55:30 -0600

    Agrega a la Lista y al Stack

--- Project-1/Project-1/Lista.h
+++ Project-1/Project-1/Lista.h
@@ -1,3 +1,131 @@
 // Lista.h
 // Autores: Jeoffrey Prado, Lorenzo Solorzano, Christian Cruz
 // Descripcion: Lista Integer, permite trabajar con datos string, int y long
+
+
+#ifndef LISTA_H
+#define LISTA_H
+
+#include <iostream>
+#include <sstream>
+#include "Nodo.h"
+
+using namespace std;
+
+template <class T> class Lista;
+template <class T>
+
+class Lista {
+private:
+	Nodo<T>* inicio;
+	int tamano;
+
+public:
+	Lista();
+	void agregar(T*);
+	//void eliminar(T*);
+	T* buscar(T*);
+	void eliminarLista();
+	void Print();
+	~Lista();
+};
+
+template <class T>
+Lista<T>::Lista() {
+	tamano = 0;
+	inicio = nullptr;
+}
+
+template <class T>
+void Lista<T>::agregar(T* dato) {
+	Nodo<T>* nuevo = new Nodo<T>;
+	nuevo->setDatos(dato);
+	nuevo->setSiguiente(nullptr);
+	if (inicio == nullptr) {
+		inicio = nuevo;
+		tamano++;
+	}
+	else {
+		nuevo->setSiguiente(inicio);
+		inicio = nuevo;
+		tamano++;
+	}
+}
+
+//template <class T>
+//void Lista<T>::eliminar(T* dato) {
+//	if (inicio != nullptr) {
+//		throw 1;
+//	}
+//	Nodo<T> auxiliar;
+//	auxiliar = inicio;
+//	if (*inicio->getDatos() == *dato) {
+//		inicio = inicio->getSiguiente();
+//		delete auxiliar;
+//	}
+//	else {
+//		Nodo<T>* auxiliar2;
+//		auxiliar2 = auxiliar->getSiguiente();
+//		while (auxiliar2 != nullptr) {
+//			auxiliar->setSiguiente(auxiliar2->getSiguiente());
+//			delete auxiliar2;
+//			break;
+//		}
+//		auxiliar = auxiliar2;
+//		auxiliar2->setSiguiente(auxiliar2->getSiguiente());
+//	}
+//	delete auxiliar;
+//}
+
+template <class T>
+T* Lista<T>::buscar(T* dato) {
+	Nodo<T>* auxilio = inicio;
+	while (!auxilio) {
+		if (dato == auxilio) {
+			auxilio->setSiguiente(auxilio->getSiguiente());
+		}
+	}
+}
+
+template <class T>
+void Lista<T>::eliminarLista() {
+	Nodo<T>* auxilio;
+	while (!inicio) {
+		auxilio = inicio;
+		inicio->setSiguiente(inicio->getSiguiente());
+		delete auxilio;
+	}
+	tamano = 0;
+}
+
+template<class T>
+inline void Lista<T>::Print()
+{
+	Nodo<T>* walk = inicio;
+	while (walk) 
+	{
+		walk->print();
+		walk = walk->getSiguiente();
+	}
+
+}
+
+
+
+template <class T>
+Lista<T>::~Lista() {
+	eliminarLista();
+}
+
+template<class T>
+inline ostream& operator << (ostream& salida, Lista<T>& l) {
+	salida << l.toString();
+	return salida;
+}
+
+
+
+
+
+#endif LISTA_H
+
