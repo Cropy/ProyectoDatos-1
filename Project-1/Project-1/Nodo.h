2d352b36e1a8723bc69be16a6b48753073285809
CruzStark <christiandacruz04@gmail.com>
2019-09-15 18:55:30 -0600

    Agrega a la Lista y al Stack

---
+++ Project-1/Project-1/Nodo.h
@@ -0,0 +1,53 @@
+#pragma once
+#ifndef NODO_H
+#define NODO_H
+
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+template <class T> class Nodo;
+
+template <class T>
+class Nodo {
+private:
+	T* datos;
+	Nodo<T>* siguiente;
+public:
+	T* getDatos();
+	Nodo* getSiguiente();
+	void setDatos(T* dato);
+	void print();
+	void setSiguiente(Nodo* siguiente);
+};
+
+template <class T>
+T* Nodo<T>::getDatos() {
+	return datos->peek();
+}
+
+template <class T>
+Nodo<T>* Nodo<T>::getSiguiente() {
+	return siguiente;
+}
+
+template <class T>
+void Nodo<T>::setDatos(T* dato) {
+	this->datos = dato;
+}
+
+template<class T>
+inline void Nodo<T>::print()
+{
+	cout << this->datos->peek();
+
+}
+
+template <class T>
+void Nodo<T>::setSiguiente(Nodo* siguiente) {
+	this->siguiente = siguiente;
+}
+
+
+#endif NODO_H
--- Project-1/Project-1/main.cpp
+++ Project-1/Project-1/main.cpp
@@ -3,6 +3,8 @@
 #include<sstream>
 #include<string>
 #include<iostream>
+#include"Lista.h"
+
 
 using namespace std;
 
@@ -10,18 +12,35 @@ using namespace std;
 int main() {
 
 	stack* s = new stack();
+	stack* s1 = new stack();
+	stack* s2 = new stack();
+	
+
+
 
 	unsigned int n1 = 22465;
-	unsigned int n2 = 22465;
-	unsigned int n3 = 22465;
+	unsigned int n2 = 22;
+	unsigned int n3 = 21;
+	unsigned int n4 = 22465;
 
 
-	s->push(n1);
+
+
+	s1->push(n1);
 	s->push(n2);
-	s->push(n3);
+	s2->push(n3);
 
-	cout << s->size() << endl;
-	cout << s->peek() << endl;
+	Lista<stack>* ListaStack = new Lista<stack>();
+
+	ListaStack->agregar(s);
+	ListaStack->agregar(s1);
+	ListaStack->agregar(s2);
+
+
+
+
+	ListaStack->Print();
+
 
 
 
